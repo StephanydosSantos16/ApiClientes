e85c000b4b5c444b79388ccbd2cea38ec236e59b
Stephany Santos <stephany.silva135@etec.sp.gov.br>
2025-07-24 05:54:14 -0300

    Atualiza CORS para incluir domínio do frontend Render

--- Program.cs
+++ Program.cs
@@ -6,18 +6,14 @@ using ApiClientes.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddSingleton<DbSession>();
-
 
 builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 builder.Services.AddScoped<IClienteService, ClienteService>();
-
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
@@ -26,27 +22,26 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173") 
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            policy.WithOrigins(
+                "http://localhost:5173",
+                "https://site-react-csyr.onrender.com"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
         });
 });
 
-
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(80); 
+    options.ListenAnyIP(80);
 });
 
 var app = builder.Build();
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseCors(MyAllowSpecificOrigins);
-
-
 
 app.MapControllers();
 
